c525ddc2fb333aa521ac32e4c7b2c56a5b890fcb
lzh <lichaohua@mocentre.com>
2020-01-19 11:08:57 +0800

    更新webview显示

--- lib/pages/pages_screen.dart
+++ lib/pages/pages_screen.dart
@@ -5,32 +5,27 @@ class PagesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   appBar: new AppBar(
-    //     title: Text('成功案例'),
-    //   ),
-    //   body: Center(
-    //     child: Text('成功案例'),
-    //   ),
-    // );
-    return new MaterialApp(
-      title: '成功案例',
-      routes: {
-        '/': (_) => new WebviewScaffold(
-          url: 'https://tb.mocentre.cn/Wap/PolyPage/index?keymark=HdzqName&Cid=0478C8570C0CFF5ECC49BAEB2E9E7DD58456BC7354ADB78E95AF51E9664C3055C7FAF1DF4740A63AC5FD13B15317F39B6B1C72928E87925F200632D84BF4AEDE87D320C095EC3EAC77AB3F8CA325B5E4C7F48586D10BA444A10C6C7652EB6785D09EF81A6F642257DB1D4075',
-          appBar: new AppBar(
-            title: const Text('凤凰嘉年华'),
-          ),
-          withZoom: true,
-          withLocalStorage: true,
-          hidden: true,
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text('凤凰嘉年华'),
+      ),
+      body: SafeArea(
+        child: WebviewScaffold(
+          url: 'https://tb.mocentre.cn/Wap/PolyPage/index?keymark=HdzqName&Cid=04E71ADB18CA006F2A3C2211673CC4C14F1C628F08368CC7EC3F27544510F3C99A11D51751BE9D9FD35D9A42434C91D769B8502A4923BF317E4F9E28E8F162060AEC7AF327F3BCB2B634F2F46DCD41350BA45F9C6820FECE7AB825C5D0AF1F163364D55F36DB4F2FAFD17771',
+          withZoom: false,  // 允许网页缩放
+          withLocalStorage: true, // 允许LocalStorage
+          withJavascript: true, // 允许执行js代码
           initialChild: Container(
             child: const Center(
               child: Text('Waiting.....'),
             ),
           ),
         ),
-      },
+        bottom: true,
+        left: true,
+        right: true,
+        top: true,
+      ),
     );
   }
 }
