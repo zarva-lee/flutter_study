37603c1d4d6dfdf0d0b727d9ba20541a545037b3
Zarva <zarva.lee@gmail.com>
2020-12-17 11:12:30 +0800

    Update pages_screen.dart

--- lib/pages/pages_screen.dart
+++ lib/pages/pages_screen.dart
@@ -11,7 +11,7 @@ class PagesScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: WebviewScaffold(
-          url: 'https://tb.mocentre.cn/Wap/PolyPage/index?keymark=HdzqName&Cid=04E71ADB18CA006F2A3C2211673CC4C14F1C628F08368CC7EC3F27544510F3C99A11D51751BE9D9FD35D9A42434C91D769B8502A4923BF317E4F9E28E8F162060AEC7AF327F3BCB2B634F2F46DCD41350BA45F9C6820FECE7AB825C5D0AF1F163364D55F36DB4F2FAFD17771',
+          url: 'xxx.baidu.com',
           withZoom: false,  // 允许网页缩放
           withLocalStorage: true, // 允许LocalStorage
           withJavascript: true, // 允许执行js代码
